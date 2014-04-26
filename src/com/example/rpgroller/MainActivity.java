3171a9ae2199e9340c52abc273ee327c0a6a1ccb
Andrew Gilbertson <agilbertson@marqeta.com>
2014-04-26 17:27:11 -0400

    Name for new attack roll screen and button onClickListeners

--- src/com/example/rpgroller/MainActivity.java
+++ src/com/example/rpgroller/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.rpgroller;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	Button attackButton;
+	Button skillButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		attackButton = (Button) findViewById(R.id.attack_button);
+		skillButton = (Button) findViewById(R.id.skill_button);
+		attackButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, AttackRollAction.class);
+				startActivity(intent);
+			}
+		});
+		skillButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, AttackRollAction.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
